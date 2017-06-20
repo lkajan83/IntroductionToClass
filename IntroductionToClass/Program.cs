a0f9e66566e46e832fca0fcec2d9cc67d66090bd
Kajan <lkajan@hotmail.com>
2017-06-20 10:17:57 +0100

    Commit

--- IntroductionToClass/Program.cs
+++ IntroductionToClass/Program.cs
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace IntroductionToClass
 {
+    /*
+     * Introduction to Classes
+     * What is classs
+     * Purpose of a class constructor
+     * Overloading class constractor
+     * understanding this keyword
+     * Destructors
+     * 
+     * Class Mean :  A Classconsisst of data and ehavior.Class Data is represented by its fields and behavor is represented by its methods
+     */
+    //Class
+    class Customer
+    {
+        string _firstName;
+        string _lastName;
+
+        public Customer() : this("No First Provided", "No LastName Provided")
+        {
+        }
+
+        //Constractor
+        public Customer(string FirstName, string LastName)
+        {
+            this._firstName = FirstName;
+            this._lastName = LastName;
+        }
+
+        //Method
+        public void PrintFullName()
+        {
+            Console.WriteLine("Full Name = {0}", this._firstName + " " + this._lastName);
+        }
+
+        //~Customer()
+        //{
+        //    //Clean Up Code
+        //}
+    }
+
     class Program
     {
-        static void Main(string[] args)
-        {
+        public static void Main()
+        { 
+            Customer C1 = new Customer();
+            C1.PrintFullName();
+
+            Customer C2 = new Customer("P","T");
+            C2.PrintFullName();     
         }
     }
 }
